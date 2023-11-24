f3b8c0d29a2ec25569f3c3a4e695aa727e8cd4b9
Sanil <sanilsarathe76@gmail.com>
2023-11-24 08:27:48 +0530

    Basic dicee setup

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Sanil Sarathe on 24/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
