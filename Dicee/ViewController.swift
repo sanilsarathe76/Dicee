27f92fe7a361fc23c0af4d122ea8f05051815b3b
Sanil <sanilsarathe76@gmail.com>
2023-12-07 22:34:16 +0530

    Dice game

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageViewFirst: UIImageView!
+    @IBOutlet weak var diceImageViewSecond: UIImageView!
+    
+    var diceFirst = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var count = 1
+    var indexSecond = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func onClickRollButton(_ sender: Any) {
+        diceImageViewFirst.image = UIImage(named: "\(diceFirst[randomValueGenerator()])")
+        diceImageViewSecond.image = UIImage(named: "\(diceFirst[randomValueGenerator()])")
+    }
+    
+    func randomValueGenerator() -> Int {
+        let randomInt = Int.random(in: 0..<6)
+        return randomInt
+    }
 }
 
